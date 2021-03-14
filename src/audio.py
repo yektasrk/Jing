9beaec219348f344c1048f113fed8aaa7910cfab
Yekta Sarkamari <yekta.sarkamari@divar.ir>
2021-03-14 12:40:17 +0330

    Add midi

--- src/audio.py
+++ src/audio.py
@@ -1,9 +1,37 @@
+import subprocess
+import time
+import mido
+
+
 class Audio:
-    def __init__():
+    self.fluid = None
+
+    def __init__(self):
+        self.fluid = subprocess.Popen(["fluidsynth", "--audio-driver=alsa",
+                                       "/usr/share/sounds/sf2/FluidR3_GM.sf2"],
+                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        out = subprocess.run(["aconnect", "-o"],
+                             capture_output=True, text=True).stdout
+        while("FLUID Synth" not in out):
+            print("wait")
+            time.sleep(1)
+            out = subprocess.run(["aconnect", "-o"],
+                                 capture_output=True, text=True).stdout
+
+        fluid_port = ""
+        ports = mido.get_output_names()
+        for port in ports:
+            if "FLUID Synth" in port:
+                fluid_port = port
+        self.port = mido.open_output(fluid_port)
+
+    def start_note(self, channel, note, loudness):
+        msg = mido.Message('note_on', note=note, velocity=loudness)
+        self.port(msg)
+
+    def stop_note(self, channel):
         pass
 
-    def start_note(channel, note, loudness):
-        pass
-
-    def stop_note(channel):
-        pass
+    def end(self):
+        self.fluid.terminate()
